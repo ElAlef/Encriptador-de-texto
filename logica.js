fc4157821ffb358b7f38ba132072795962dd001d
ElAlef <ben-ale@hotmail.com>
2024-02-21 00:15:55 -0300

    agregando funcion desencriptar

--- logica.js
+++ logica.js
@@ -1,7 +1,7 @@
 function encriptar () {
     let texto = document.getElementById("texto").value;
     let tituloMensaje = document.getElementById("titulo-mensaje");
-    // let parrafo = document.getElementById("parrafo");
+    let parrafo = document.getElementById("parrafo");
     let muneco = document.getElementById("muneco");
 
     let textoCifrado = texto 
@@ -11,7 +11,7 @@ function encriptar () {
       .replace(/o/gi, "ober")                  
       .replace(/u/gi, "ufat");
 
-    if (texto.value.length != 0) {
+    if (texto.length != 0) {
         document.getElementById("texto").value = textoCifrado;
         tituloMensaje.textContent = "Texto encriptado con éxito";
         parrafo.textContent = "";
@@ -23,6 +23,29 @@ function encriptar () {
 
        alert("Debes ingresar algún texto")
     }
+}
 
-
+function desencriptar() {
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let muneco = document.getElementById("muneco");
+      
+    let textoCifrado = texto
+      .replace(/enter/gi, "e")
+      .replace(/imes/gi, "i")
+      .replace(/ai/gi, "a")
+      .replace(/ober/gi, "o")
+      .replace(/ufat/gi, "u");
+        
+    if (texto.length != 0) {
+      document.getElementById("texto").value = textoCifrado;
+      tituloMensaje.textContent = "Texto desencriptado con éxito";
+      parrafo.textContent = "";            
+      muneco.src = "./img/desencriptado.jpg";
+    } else {
+      muneco.src = "./img/Muneco.png";
+      tituloMensaje.textContent = "Ningún mensaje fue encontrado";            parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+      alert("Ooops! Debes ingresar un texto");
+    }
 }
